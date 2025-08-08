07e03d5bd395a52125351ce98ecfa2d73305bd76
redlemon8 <hanquezbenjamin@gmx.com>
2025-08-08 22:22:40 +0200

    data: add seeding tags

--- src/migrations/seedTables.js
+++ src/migrations/seedTables.js
@@ -8,6 +8,13 @@ const projectName = await Card.create({ content: "Wipflow Project", position: 1,
 
 console.log("🚧 Add testing tag...");
 const urgentTag = await Tag.create({ name: "Urgent", color: "#FF0000"});
+const importantTag = await Tag.create({ name: "Important", color: "#00FF00"});
+const inProgressTag = await Tag.create({ name: "En cours", color: "#FFFF00"});
+const doneTag = await Tag.create({ name: "Terminé", color: "#00FF00"});
+const toDoTag = await Tag.create({ name: "À faire", color: "#118ab2"});
+const toValidateTag = await Tag.create({ name: "À valider", color: "#00d9ff"});
+const toRefineTag = await Tag.create({ name: "À améliorer", color: "#646cff"});
+
 
 console.log("🚧 Add tag on card...");
 await projectName.addTag(urgentTag);
